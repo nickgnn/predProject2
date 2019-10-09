366af64259a115beeedb723fc77ddbf88693ba50
nick <gnn@mm.st>
2019-10-09 21:06:02 +0300

    remove redundant imports

--- src/main/java/dao/UserDaoByJDBC.java
+++ src/main/java/dao/UserDaoByJDBC.java
@@ -1,9 +1,11 @@
 package dao;
 
 import model.User;
-import sun.dc.pr.PRError;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
